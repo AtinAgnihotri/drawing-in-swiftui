fb4ad9f7d536308095057789ce0237abe04ba262
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-15 23:38:33 +0530

    Initial Commit

---
+++ drawing-in-swiftui/drawing_in_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawing_in_swiftuiApp.swift
+//  drawing-in-swiftui
+//
+//  Created by Atin Agnihotri on 15/06/21.
+//
+
+import SwiftUI
+
+@main
+struct drawing_in_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
