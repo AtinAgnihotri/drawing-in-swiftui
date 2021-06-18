c59f9496df3d04b8fe6d4a3667c98e08eea478f2
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-18 20:14:43 +0530

    Transforming Shapes with CGAffineTransform

--- drawing-in-swiftui/TransformingShapes.swift
+++ drawing-in-swiftui/TransformingShapes.swift
@@ -7,9 +7,52 @@
 
 import SwiftUI
 
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = 20
+    // How wide to make each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: CGFloat(0), to: CGFloat.pi * 2, by: CGFloat.pi/8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width/2))
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            // Add it to our original path
+            path.addPath(rotatedPetal)
+        }
+        return path
+    }
+}
+
 struct TransformingShapes: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+//                .stroke(Color.red, lineWidth: 2)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+                .transition(.scale)
+//            TextField
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+            
+        }
     }
 }
 
